68177e5f190ba71945285c8f028814b55fb636b2
xavier zebier <xavier.ernest.zebier@gmail.com>
2016-09-26 21:20:01 +0200

    added test to right case

--- handlers/auth_handler_test.go
+++ handlers/auth_handler_test.go
@@ -1,6 +1,7 @@
 package handlers_test
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -65,6 +66,35 @@ func TestUnUserHandler(t *testing.T) {
 	if status := rr.Code; status != http.StatusUnauthorized {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusOK)
+	}
+}
+
+func TestOkHandler(t *testing.T) {
+
+	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
+	// pass 'nil' as the third parameter.
+	req, err := http.NewRequest("GET", "/login", nil)
+	req.SetBasicAuth("user", "password")
+	if err != nil {
+		t.Fatal(err)
+	}
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+	handler := handlers.LoginHandler("secret", users)
+
+	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+	// directly and pass in our Request and ResponseRecorder.
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+	var token handlers.LoginResponse
+	err = json.Unmarshal(rr.Body.Bytes(), &token)
+	if err != nil {
+		t.Errorf("unable to unmarshall the response")
 	}
 
 }
