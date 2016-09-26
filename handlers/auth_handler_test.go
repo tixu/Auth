facfc9914e7368ad1dc8c87e57d834a0dc1ff2c2
xavier zebier <xavier.ernest.zebier@gmail.com>
2016-09-26 15:29:13 +0200

    added unauthorized test

---
+++ handlers/auth_handler_test.go
@@ -0,0 +1,47 @@
+package handlers_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/tixu/Auth/handlers"
+)
+
+var users handlers.Users
+
+func init() {
+	users = handlers.Users{
+		"user": handlers.User{
+			Username: "user",
+			// bcrypt has for "password"
+			PasswordHash: "$2a$10$KgFhp4HAaBCRAYbFp5XYUOKrbO90yrpUQte4eyafk4Tu6mnZcNWiK",
+			Email:        "user@example.com",
+			Role:         "wtfd",
+		},
+	}
+}
+
+func TestAuhthHandler(t *testing.T) {
+
+	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
+	// pass 'nil' as the third parameter.
+	req, err := http.NewRequest("GET", "/login", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+	handler := handlers.LoginHandler("secret", users)
+
+	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+	// directly and pass in our Request and ResponseRecorder.
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusUnauthorized {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+}
