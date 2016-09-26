56f4975cfe44ebe08e80fc0984074e6d98f2971b
xavier zebier <xavier.ernest.zebier@gmail.com>
2016-09-26 15:33:37 +0200

    add unknow user

--- handlers/auth_handler_test.go
+++ handlers/auth_handler_test.go
@@ -22,11 +22,34 @@ func init() {
 	}
 }
 
-func TestAuhthHandler(t *testing.T) {
+func TestUnAuhthHandler(t *testing.T) {
 
 	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
 	// pass 'nil' as the third parameter.
 	req, err := http.NewRequest("GET", "/login", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+	handler := handlers.LoginHandler("secret", users)
+
+	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+	// directly and pass in our Request and ResponseRecorder.
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusUnauthorized {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+}
+func TestUnUserHandler(t *testing.T) {
+
+	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
+	// pass 'nil' as the third parameter.
+	req, err := http.NewRequest("GET", "/login", nil)
+	req.SetBasicAuth("xavier", "res")
 	if err != nil {
 		t.Fatal(err)
 	}
