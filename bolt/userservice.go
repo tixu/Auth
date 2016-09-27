6ea26d8ca723fedaeff2e04e1ab59e93064d2d3d
xavier zebier <xavier.ernest.zebier@gmail.com>
2016-09-27 22:14:42 +0200

    Implement the final touch of get user

--- bolt/userservice.go
+++ bolt/userservice.go
@@ -1,6 +1,10 @@
 package bolt
 
-import "github.com/tixu/Auth/users"
+import (
+	"github.com/pkg/errors"
+	"github.com/tixu/Auth/bolt/internal"
+	"github.com/tixu/Auth/users"
+)
 
 // DialService represents a service for managing dials.
 
@@ -21,11 +25,11 @@ func (s *UserService) GetUser(name string) (users.User, error) {
 	var u users.User
 
 	if v := tx.Bucket([]byte(userBucket)).Get([]byte(name)); v == nil {
+		err := errors.New("No Data Found")
+		return u, err
+	} else if err := internal.UnmarshalUser(v, &u); err != nil {
 
+		return u, errors.Wrap(err, "unable to unmarshall retrieve information")
 	}
-
-	// else if err := internal.UnmarshalDial(v, &u); err != nil {
-	//	return nil, err
-	// }
 	return u, nil
 }
