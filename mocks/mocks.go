3684dc5cd42bca78a7c926d9165425c5eb1f65d9
xavier zebier <xavier.ernest.zebier@gmail.com>
2016-10-05 08:42:39 +0200

    fixed the ID problem

--- mocks/mocks.go
+++ mocks/mocks.go
@@ -15,6 +15,7 @@ var DB = users.Users{
 		PasswordHash: "$2a$10$KgFhp4HAaBCRAYbFp5XYUOKrbO90yrpUQte4eyafk4Tu6mnZcNWiK",
 		Email:        "user@example.com",
 		Role:         "wtfd",
+		ID:           3,
 	},
 	"admin": users.User{
 		Name: "admin",
@@ -23,11 +24,26 @@ var DB = users.Users{
 		PasswordHash: "$2a$08$5XdTvZR/PaCsVbQYNcDuAeZ6P.lL75VC1Z819M6myutcnasfKZSiq",
 		Email:        "user@example.com",
 		Role:         "wtfd",
+		ID:           456,
 	},
 }
 
 type UserService struct {
 	DB users.Users
+}
+
+func (*UserService) AddUser(user *users.User) error {
+	DB[user.Name] = *user
+	return nil
+}
+
+func (*UserService) DeleteUser(name string) error {
+	delete(DB, name)
+	return nil
+}
+
+func (*UserService) ListAll() (users.Users, error) {
+	return DB, nil
 }
 
 func (*UserService) GetUser(name string) (*users.User, error) {
@@ -41,15 +57,12 @@ func (*UserService) GetUser(name string) (*users.User, error) {
 
 func GetUserMockService() *UserService {
 	return &UserService{
-		DB: users.Users{
-			"user": users.User{
-				Name: "user",
-				// bcrypt has for "password"
-				PasswordHash: "$2a$10$KgFhp4HAaBCRAYbFp5XYUOKrbO90yrpUQte4eyafk4Tu6mnZcNWiK",
-				Email:        "user@example.com",
-				Role:         "wtfd",
-			},
-		},
+		DB: DB,
 	}
+}
 
+func GetAdminMockService() *UserService {
+	return &UserService{
+		DB: DB,
+	}
 }
